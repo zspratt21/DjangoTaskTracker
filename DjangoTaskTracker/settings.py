b628ca4a6f5900c4c8468ebf8bb2a61887873c65
Zak <zspratt21@gmail.com>
2023-10-24 18:42:48 +0800

    fixed up docker environment and connected postgres to django container.

--- DjangoTaskTracker/settings.py
+++ DjangoTaskTracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'DjangoTaskTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_DATABASE'),
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'postgres',  # Use the name of the service in the docker-compose file
+        'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
+
 
 
 # Password validation
